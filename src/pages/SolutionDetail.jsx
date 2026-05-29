c89f587383a81c023b067f0b5051adcdf61b3477
DAVT-2008 <avstorelk.davt@gmail.com>
2026-05-29 12:32:54 +0530

    Update SolutionDetail.jsx

--- src/pages/SolutionDetail.jsx
+++ src/pages/SolutionDetail.jsx
@@ -24,7 +24,7 @@ const solutionsData = {
       {
         name: "Gampaha Complex",
         details: "Multi-screen audio-visual synchronization and lobby digital signage networking for a modern multiplex feel.",
-        photo: "https://res.cloudinary.com/dtscqhcop/image/upload/v1764137791/grid3_dnzliw.jpg"
+        photo: "https://thfvnext.bing.com/th/id/OIP.VWeTaorW4fITAGFy0Z2hXwHaFj?w=269&h=201&c=7&r=0&o=7&cb=thfvnextfalcon&dpr=1.3&pid=1.7&rm=3"
       },
             {
         name: "Dematagoda Complex",
